9aa8afe6b7dd99224df46d2cce5a6d47d198a799
yoojinPPPark <101375618+yoojinPPPark@users.noreply.github.com>
2022-04-07 22:53:39 +0900

    update for numarry in cal.sh

--- cal.sh
+++ cal.sh
@@ -3,35 +3,36 @@
 #"shel script file read" keyword로 search
 #선택된 paramater로 계산 수행
 
-num1='cat num1.txt'
-num2='cat num2.txt'
+num1=$(<cat num1.txt)
+num2=$(<cat num2.txt')
+numarry=("$num1" "$num2) 
 
 final_calculate(){
 
-        if ["$parameter" = add];then
-                result = $num1 + $num2
-        elif ["$parameter" = sub];then
-                result = $num1 - $num2
-        elif ["$parameter" = div]; then
-                result = $num1/$num2
-        elif ["$parameter" = mul]; then
-                result = $num1*$num2
+        if [ "$parameter" = "add" ]; then
+                let result = $numarry[0] + $numarray[1]
+        elif [ "$parameter" = "sub" ]; then
+                let result = $numarry[0] - $numarray[1]
+        elif [ "$parameter" = "div" ]; then
+                let result = $numarry[0] / $numarray[1]
+        elif [ "$parameter" = "mul"]; then
+                let result = $numarry[0] /* $numarray[1]
         else
                 echo "...none operator parameter..."
                 echo "There are 4selections you can choose"
                 menu=("add" "sub" "div" "mul")
-                echo "1)add 2)sub 3)div 4)mul
+                echo "1)add 2)sub 3)div 4)mul"
                 read -p  "Select menu: " num
-                parameter=$num
+                parameter=${menu[num - 1]}
                 echo $parameter
                 echo "run calculater"
         fi
-        echo "num1: 35"
-        echo "num2: 27"
+        echo "num1: $num1"
+        echo "num2: $num2"
         echo "op: $parameter"
         echo "result: $result"
 
 }
-commands
+
 final_calculate
 
